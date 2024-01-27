b6015e658d770a99d8f713cf45a4ab20fe3e03d2
Daigo Shitara <shitara@tagbangers.co.jp>
2024-01-27 19:51:57 +0900

    fix move files

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/v2.2.0',
+        permanent: true,
+      },
+    ]
+  },
+})
